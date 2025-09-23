6e53348c0aa72d076f0d7037aee807f0cd5fef88
chaves <chavesprogrammer@gmail.com>
2025-09-23 15:45:02 -0300

    first commit

---
+++ src/Server.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class Server {
+
+
+    public Server(int port){
+
+        try(ServerSocket ss = new ServerSocket(port)){
+
+            System.out.println("Server startou na porta: " + port);
+
+            while (true){
+                System.out.println("Esperando conexão com cliente");
+
+                Socket s = ss.accept();
+
+                System.out.println("Conexão aceita: " + s.getInetAddress());
+
+                OutputStream outputStream = s.getOutputStream();
+
+                String message = "Hello World";
+
+                int lenght = getLengthStringToBytes(message);
+
+                String httpResponse = String.format("""
+                        HTTP/1.1 200 OK\r
+                        Content-Type: text/plain\r
+                        Content-Length: %d\r
+                        \r
+                        """,
+                        lenght);
+
+                outputStream.write(httpResponse.getBytes(StandardCharsets.UTF_8));
+
+                outputStream.flush();
+
+                outputStream.write(message.getBytes(StandardCharsets.UTF_8));
+
+                outputStream.flush();
+
+                System.out.println("Fechando conexão");
+
+                s.close();
+            }
+        } catch (IOException e) {
+            System.out.println("Erro:" + e);
+        }
+
+    }
+
+    private int getLengthStringToBytes(String msg){
+        byte[] msgBytes = msg.getBytes(StandardCharsets.UTF_8);
+        return msgBytes.length;
+    }
+
+}
